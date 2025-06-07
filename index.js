f094473b3563f0f17e0f70e626478f0444a910d4
Garvit Asthana <garvit.asthana@gmail.com>
2025-06-07 13:51:10 +0530

    s

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from EC2 test app!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
