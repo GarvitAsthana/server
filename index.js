6ac84f0de5b50b416add5246ae61bbe46315149b
Garvit Asthana <garvit.asthana@gmail.com>
2025-06-07 12:49:03 +0530

    express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("jai shree ram")
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from EC2 test app!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
